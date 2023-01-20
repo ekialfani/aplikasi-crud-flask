f4b95b74298087e9fb52947ffd12641d6ddeca91
Eki Alfani <ekialfani15@gmail.com>
2023-01-20 23:16:43 +0800

    flask message

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 # url_for untuk handel URL dan redirect untuk mengarahkan ke route tertentu
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, session, flash
 
 app = Flask(__name__)
+app.secret_key = 'asdfghjkl12345fdsa_fdsakld8rweodfds'
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
   if request.method == 'POST':
-    return redirect(url_for('dosen'))
+    if request.form['email'] != 'ekialfani@gmail.com' and request.form['password'] != 'eki123':
+      flash('Invalid username/password', 'danger')
+    else:
+      session['logged_in'] = True
+      flash('Login successful', 'success')
+      return redirect(url_for('dosen'))
   return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+  session.pop('logged_in', None)
+  flash('Logout successful', 'success')
+  return redirect(url_for('login'))
 
 @app.route('/dosen')
 def dosen():
