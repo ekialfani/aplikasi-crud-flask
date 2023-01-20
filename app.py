cf5bc8b0b2e793bc5d69271cf1c73fd693eb60a7
Eki Alfani <ekialfani15@gmail.com>
2023-01-21 06:30:55 +0800

    tambah, edit dan hapus data

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ host = 'localhost'
 user = 'root'
 password = ''
 db = 'universitas'
-conn = pymysql.connect(host=host,user=user,password=password,db=db,)
+mysql = pymysql.connect(host=host,user=user,password=password,db=db,)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -32,11 +32,82 @@ def logout():
 
 @app.route('/dosen')
 def dosen():
-  cursor = conn.cursor()
+  cursor = mysql.cursor()
   cursor.execute(''' SELECT * FROM dosen ''')
   dosen = cursor.fetchall()
   cursor.close()
   return render_template('dosen.html', dosen=dosen)
+
+
+@app.route('/dosen/tambah', methods=['GET', 'POST'])
+def tambahDosen():
+  if request.method == 'GET':
+    return render_template('dosen/add.html')
+  else:
+    nama = request.form['nama']
+    univ = request.form['univ']
+    jurusan = request.form['jurusan']
+
+    cursor = mysql.cursor()
+    cursor.execute(''' INSERT INTO dosen(nama, univ, jurusan) VALUES(%s,%s,%s) ''',(nama,univ,jurusan))
+    mysql.commit()
+    cursor.close()
+    flash('Data added successfully', 'success')
+    return redirect(url_for('dosen'))
+  return render_template('dosen.html')
+
+
+@app.route('/dosen/edit/<int:id>', methods=['GET', 'POST'])
+def editdosen(id):
+  if request.method == 'GET':  
+    cursor = mysql.cursor()
+    cursor.execute('''
+    SELECT * 
+    FROM dosen 
+    WHERE dosen_id=%s''', (id, ))
+    dosen = cursor.fetchone()
+    cursor.close()
+
+    return render_template('dosen/edit.html', dosen=dosen)
+  else:
+    nama = request.form['nama']
+    univ = request.form['univ']
+    jurusan = request.form['jurusan']
+
+    cursor = mysql.cursor()
+    cursor.execute(''' 
+    UPDATE dosen 
+    SET 
+        nama = %s,
+        univ = %s,
+        jurusan = %s
+    WHERE
+        dosen_id = %s;
+    ''',(nama,univ,jurusan,id))
+    
+    mysql.commit()
+    cursor.close()
+    flash('Data updated successfully','success')
+    return redirect(url_for('dosen'))
+
+  return render_template('dosen.html')
+
+
+@app.route('/dosen/delete/<int:id>', methods=['GET'])
+def deletedosen(id):
+  if request.method == 'GET':
+    cursor = mysql.cursor()
+    cursor.execute('''
+    DELETE 
+    FROM dosen 
+    WHERE dosen_id=%s''', (id, ))
+    mysql.commit()
+    cursor.close()
+
+    return redirect(url_for('dosen'))
+
+  return render_template('dosen.html')
+
 
 if __name__ == '__main__':
   app.run(debug=True)
