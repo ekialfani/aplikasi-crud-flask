d6b6a773d419bb6e9b8d67415f0e0d4b272e9273
Eki Alfani <ekialfani15@gmail.com>
2023-01-20 20:12:35 +0800

    menambahkan bootstrap pada flask

--- app.py
+++ app.py
@@ -1,52 +1,17 @@
-from flask import Flask, render_template
+# url_for untuk handel URL dan redirect untuk mengarahkan ke route tertentu
+from flask import Flask, render_template, url_for, redirect, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return "hello world"
-
-# routing
-# statis
-@app.route('/nama')
-def nama():
-  return "halaman nama"
-
-# dinamis
-@app.route('/nama/<string:nama>')
-def getNama(nama):
-  return "nama anda adalah: {}".format(nama);
-
-
-# auto reload
-# jalankan perintah ini di terminal/cmd
-# export FLASK_ENV=development (Untuk Linux)
-# set FLASK_ENV=development (Set Windows)
-
-
-# render html
-@app.route('/mahasiswa')
-def getMahasiswa():
-  return "<h1>Data Mahasiswa</h1>"
-
-
-# template
-# jinja template
-# {% ... %} untuk Pernyataan seperti IF dan FOR
-# {{ ... }} untuk mencetak variabel/ekspresi ke template
-# {# ... #} untuk Komentar yang tidak disertakan 
-
-@app.route('/templates')
-def templateMahasiswa():
-  kelas = "IF-4201"
-  return render_template('mahasiswa.html', kelas=kelas)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+    return redirect(url_for('dosen'))
+  return render_template('login.html')
 
 @app.route('/dosen')
-def getDosen():
-  hobbies = ['membaca', 'jalan-jalan', 'nonton']
-  return render_template('dosen.html', hobbies=hobbies)
-
-
+def dosen():
+  return render_template('dosen.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
