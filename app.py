f7df754f159184e15243058d07d2b8fb87295660
Eki Alfani <ekialfani15@gmail.com>
2023-01-21 00:26:58 +0800

    konfigurasi mysql dan mengambil data dari database

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 # url_for untuk handel URL dan redirect untuk mengarahkan ke route tertentu
 from flask import Flask, render_template, url_for, redirect, request, session, flash
+import pymysql
+
 
 app = Flask(__name__)
 app.secret_key = 'asdfghjkl12345fdsa_fdsakld8rweodfds'
 
+# mysql config
+host = 'localhost'
+user = 'root'
+password = ''
+db = 'universitas'
+conn = pymysql.connect(host=host,user=user,password=password,db=db,)
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
   if request.method == 'POST':
-    if request.form['email'] != 'ekialfani@gmail.com' and request.form['password'] != 'eki123':
+    if request.form['username'] != 'admin' and request.form['password'] != '123':
       flash('Invalid username/password', 'danger')
     else:
       session['logged_in'] = True
@@ -23,7 +32,11 @@ def logout():
 
 @app.route('/dosen')
 def dosen():
-  return render_template('dosen.html')
+  cursor = conn.cursor()
+  cursor.execute(''' SELECT * FROM dosen ''')
+  dosen = cursor.fetchall()
+  cursor.close()
+  return render_template('dosen.html', dosen=dosen)
 
 if __name__ == '__main__':
   app.run(debug=True)
