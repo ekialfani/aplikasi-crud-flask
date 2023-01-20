e482e3fe1f053b9caeffe2fd1e13d71673928ce8
Eki Alfani <ekialfani15@gmail.com>
2023-01-20 18:15:47 +0800

    routing statis dan dinamis

--- app.py
+++ app.py
@@ -6,5 +6,16 @@ app = Flask(__name__)
 def index():
   return "hello world"
 
+# routing
+# statis
+@app.route('/nama')
+def nama():
+  return "halaman nama"
+
+# dinamis
+@app.route('/nama/<string:nama>')
+def getNama(nama):
+  return "nama anda adalah: {}".format(nama);
+
 if __name__ == '__main__':
   app.run(debug=True)
