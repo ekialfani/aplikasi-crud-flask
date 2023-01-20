5010588b81fe4343c43ebb28c779acbbf846ba61
Eki Alfani <ekialfani15@gmail.com>
2023-01-20 18:20:36 +0800

    auto reload

--- app.py
+++ app.py
@@ -17,5 +17,11 @@ def nama():
 def getNama(nama):
   return "nama anda adalah: {}".format(nama);
 
+
+# auto reload
+# jalankan perintah ini di terminal/cmd
+# export FLASK_ENV=development (Untuk Linux)
+# set FLASK_ENV=development (Set Windows)
+
 if __name__ == '__main__':
   app.run(debug=True)
